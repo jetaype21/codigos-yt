f79a1b5669a42923101e2ac1dd974a9b9ff3133c
taype dev <jetaypedev@gmail.com>
2023-09-21 05:30:16 -0500

    first project for yt

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import books from "./assets/books.json" assert { type: "json" };
+
+const app = express();
+app.use(express.json());
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+// ruta para books
+app.get("/books", (req, res) => {
+  // devolver todos los libros
+  res.json(books);
+});
+
+app.listen(port, () => {
+  console.log(`el servidor corriendo en el puerto ${port}!`);
+});
